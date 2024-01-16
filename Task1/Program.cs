623bc65e398d1d9459cd3c433b3715cb340a6bd2
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-16 15:52:23 +0300

    Completed task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число кратное 7 и 23 одновременно:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%7==0 && a%23==0)
+{
+    Console.WriteLine("Условие выполнено!");
+}
+else
+{
+    Console.WriteLine("Введенное число не кратно одновременно 7 и 23 одновременно! Условие не выполнено!");
+}
