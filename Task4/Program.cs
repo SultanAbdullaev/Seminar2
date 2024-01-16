1eb03367b82d0e369dc716de4ef14164731dd62f
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-16 16:37:37 +0300

    Completed task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int mod;
+while(num!=0)
+{
+    mod=num%10;
+    Console.Write(mod);
+    num/=10;
+    if (num==0)
+    {
+        Console.WriteLine("\n");
+        break;
+    }
+    Console.Write(", ");
+}
