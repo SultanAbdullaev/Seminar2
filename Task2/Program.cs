33e558e134999809fa8ac18799d5faa7b64de604
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-16 16:09:07 +0300

    Completed task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите координату Х не равную нулю:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y не равную нулю:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x!=0&&y!=0){
+if(x>0&&y>0)
+{
+    Console.WriteLine("1-я четверть координатной плоскости!");
+}
+else if(x<0&&y>0)
+{
+    Console.WriteLine("2-я четверть координатной плоскости!");
+
+}
+else if(x<0&&y<0)
+{
+    Console.WriteLine("3-я четверть координатной плоскости!");
+
+}
+else
+{
+    Console.WriteLine("4-я четверть координатной плоскости!");
+
+}
+}
+else
+{
+    Console.WriteLine("X и Y не должны быть равны нулю");
+}
